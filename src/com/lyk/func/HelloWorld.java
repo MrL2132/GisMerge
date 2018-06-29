fa8a14ab8298b06ebd9c87ab537fb71db3e99925
fireclone <cozy4580@126.com>
2018-06-29 18:34:01 +0800

    Hai v1.0

--- src/com/lyk/func/HelloWorld.java
+++ src/com/lyk/func/HelloWorld.java
@@ -3,5 +3,6 @@ package com.lyk.func;
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("hello world!!!!(LYK)");
+	System.out.println("hello world!!!!(LYK)");
 }
 }
