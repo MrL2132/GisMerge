d08374e3fe4513f4381d5c08d274d7a23b2e25ec
MrL2132 <MrL2132@126.com>
2018-06-29 18:37:27 +0800

    Hai V1.01

--- src/com/lyk/func/HelloWorld.java
+++ src/com/lyk/func/HelloWorld.java
@@ -3,5 +3,7 @@ package com.lyk.func;
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("hello world!!!!(LYK)");
+	
+	System.out.println("hello world again!!!!(LYK)");
 }
 }
